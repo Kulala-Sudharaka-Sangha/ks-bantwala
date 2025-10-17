7e2f8b48bec76c8f13bc9dc2b6d67dd27b8f9d1e
premnathkulal <premnathkulal1998@gmail.com>
2025-10-17 16:51:28 +0530

    Updated teachers section

--- src/components/teachers-section/TeachersSection.tsx
+++ src/components/teachers-section/TeachersSection.tsx
@@ -1,6 +1,25 @@
 import "./TeachersSection.scss";
 
 export const TeachersSection = () => {
+  const teachers = [
+    {
+      name: "Shri Chennakeshava Kulal",
+      role: "Drawing Teacher",
+      image: "https://picsum.photos/160/160?random=1",
+    },
+    {
+      name: "Shri Mahesh Kulal Kadeswalya",
+      role: "Dance Teacher",
+      image:
+        "https://i.ibb.co/0yW5Qf75/Screenshot-2025-10-17-at-4-36-23-PM.png",
+    },
+    {
+      name: "Smt Sowmya S Kulal",
+      role: "Bhajan Teacher",
+      image: "https://picsum.photos/160/160?random=3",
+    },
+  ];
+
   return (
     <div className="teachers-section">
       <div className="section-header">
@@ -13,59 +32,23 @@ export const TeachersSection = () => {
         refine your skills, our teachers provide personalized attention and
         support to ensure your success.
       </div>
+
       <div className="teachers-list-container">
-        <div className="teacher-info">
-          <div className="teacher-image-container">
-            <img
-              src="https://picsum.photos/160/160?random=2"
-              alt="Teacher"
-              className="teacher-image"
-            />
+        {teachers.map((teacher, index) => (
+          <div key={index} className="teacher-info">
+            <div className="teacher-image-container">
+              <img
+                src={teacher.image}
+                alt={teacher.name}
+                className="teacher-image"
+              />
+            </div>
+            <div className="teacher-name">
+              {teacher.name}
+              <div className="teacher-role">{teacher.role}</div>
+            </div>
           </div>
-          <div className="teacher-name">
-            Mahesh Kulal Kadeswalya
-            <div className="teacher-role">Dance Teacher</div>
-          </div>
-        </div>
-        <div className="teacher-info">
-          <div className="teacher-image-container">
-            <img
-              src="https://picsum.photos/160/160?random=2"
-              alt="Teacher"
-              className="teacher-image"
-            />
-          </div>
-          <div className="teacher-name">
-            Mahesh Kulal Kadeswalya
-            <div className="teacher-role">Dance Teacher</div>
-          </div>
-        </div>
-        <div className="teacher-info">
-          <div className="teacher-image-container">
-            <img
-              src="https://picsum.photos/160/160?random=2"
-              alt="Teacher"
-              className="teacher-image"
-            />
-          </div>
-          <div className="teacher-name">
-            Mahesh Kulal Kadeswalya
-            <div className="teacher-role">Dance Teacher</div>
-          </div>
-        </div>
-        <div className="teacher-info">
-          <div className="teacher-image-container">
-            <img
-              src="https://picsum.photos/160/160?random=2"
-              alt="Teacher"
-              className="teacher-image"
-            />
-          </div>
-          <div className="teacher-name">
-            Mahesh Kulal Kadeswalya
-            <div className="teacher-role">Dance Teacher</div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
