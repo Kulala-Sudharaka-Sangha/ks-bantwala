81906abb7691179fdc178a3f29f4c3d8f2daaee5
premnathkulal <premnathkulal1998@gmail.com>
2025-10-17 13:56:23 +0530

    Fixed menu bar bottom border style issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.scss";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import MasterMenu from "./components/menu/MasterMenu";
 import Header from "./components/header/Header";
+import { useEffect } from "react";
+import { setActiveRouterPage } from "./store/slices/ui-controls";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(setActiveRouterPage(location.pathname));
+  }, []);
+
   return (
     <div className="app">
       <Header />
