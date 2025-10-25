52a0e63fc928ca6ed9a38f80016daf5e5cac2a6f
premnathkulal <premnathkulal1998@gmail.com>
2025-10-25 15:48:27 +0530

    Updated gallery images

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -6,9 +6,9 @@ const Gallery = () => {
     description: "Explore our collection of images and videos.",
     images: [
       // Add 200 more images to the gallery
-      ...Array.from({ length: 25 }, (_, i) => ({
+      ...Array.from({ length: 22 }, (_, i) => ({
         id: i,
-        src: `/sample-images/horse-${i + 1}.jpg`,
+        src: `/gallery-images/kulala-${i + 1}.webp`,
       })),
     ],
   };
@@ -16,6 +16,10 @@ const Gallery = () => {
   return (
     <div className="gallery">
       <div className="page-title">Moments & Memories</div>
+      {/* <p>
+        Explore our collection of images. Enjoy the visual journey through our
+        gallery.
+      </p> */}
       <div className="gallery-content">
         {galleryData.images.map((image) => (
           <img
